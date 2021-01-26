e987e2a90172aadc047be208254130b9edfa512a
Linar Valdianov <v.linar0711@mail.ru>
2021-01-27 02:17:05 +0500

    add header menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "@/components/header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
 
 * {
-  font-family: "Open Sans", sans-serif;
+  font-family: "Open Sans";
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: 300;
 }
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header-menu">
+    <a href="#" class="active">Магазин</a>
+    <a href="#">О нас</a>
+    <img
+      src="https://www.romanticanawear.com/dist/images/d90e4fe0a0e5c48daaee65edefda339b.svg"
+      alt="Logo"
+    />
+    <a href="#">Конакты</a>
+    <a href="#">Корзина (0)</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+$activecolor: red;
+
+.header-menu {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2% 20%;
+  max-width: 1440px;
+  a {
+    text-decoration: none;
+    color: black;
+    border-bottom: 1px solid #00000000;
+    line-height: 23px;
+  }
+  a:hover {
+    color: $activecolor;
+  }
+  .active {
+    color: $activecolor;
+    border-bottom: 1px solid $activecolor;
+  }
+}
+</style>
